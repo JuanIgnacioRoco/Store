d84757e854107da0517cdb916f48fc5d5c98869c
Juan Ignacio Roco <juan.roco.aguirre@ciisa.cl>
2020-01-30 15:46:35 -0300

    progresando 2

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
+from carts.models import CartProducts
 from carts.utils import get_or_create_cart
 from products.models import Product
 
@@ -13,7 +15,14 @@ def cart(request):
 def add(request):
     cart = get_or_create_cart(request)
     product = Product.objects.get(pk=request.POST.get('product_id'))
-    cart.products.add(product)
+    quantity = int(request.POST.get('quantity', 1))
+
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, product=product, quantity=quantity)
+
     return render(request, 'carts/add.html', {
         'product': product
     })
